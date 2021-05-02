83a951dbda57c24e283d0815ed2bcd02e6bb69ce
pwdfaruk <you@example.com>
2021-05-02 14:20:50 +0600

    adding background images

--- asset/js/main.js
+++ asset/js/main.js
@@ -101,3 +101,25 @@ $('.owl-testi').owlCarousel({
                 }
             }
 	});
+/* post slider carousel */
+/* owl carousel */
+$('.post-slider').owlCarousel({
+            autoplay: true,
+            autoplayTimeout:5000,
+            autoplayHoverPause:true,
+            loop:true,
+            margin:10,
+            smartSpeed: 800,
+            nav:true,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:1
+                },
+                1000:{
+                    items:3
+                }
+            }
+    });
